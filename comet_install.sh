64f3e26f2fa12e3851db8518ad5406d76b0c529b
elppans <elppansmk@gmail.com>
2020-08-19 00:18:32 -0300

    Add files via upload

--- comet_install.sh
+++ comet_install.sh
@@ -5,7 +5,7 @@ BIN='/usr/bin'
 LBIN='/usr/local/bin'
 NODE='/usr/local/node/bin'
 LINKCMT='https://github.com/elppans/zretail/raw/master'
-COMET='comet_zanthus_1.0.1.4_254.v3.tar.xz'
+COMET='comet_zanthus_1.0.1.4_254.v4.tar.xz'
 HTDOCS='/usr/local/apache22/htdocs/'
 HTCOMET=""$HTDOCS"comet_zanthus"
 PATH=""$HTDOCS"src"
@@ -20,17 +20,46 @@ tar -zxvf "$COMET" -C "$HTDOCS"
 ln -sfv "$NODE"/* "$BIN"
 ln -sfv  "$NODE"/* "$LBIN"
 sed -i "s/"$IPSERV"/127.0.0.1/g" "$PATHCFG"
-ln -sf "$HTCOMET"/comet_zanthus /etc/init.d/comet_zanthus
 ln -sf "$HTCOMET"/comet_zanthus "$BIN"
-chkconfig comet_zanthus on
+ln -sf "$HTCOMET"/comet_clear-log.sh /etc/cron.daily/
+
+
+
+# Serviço init.d:
+#ln -sf "$HTCOMET"/comet_zanthus /etc/init.d/comet_zanthus
+#chkconfig comet_zanthus on
+#systemctl enable comet_zanthus
+#service comet_zanthus start
+
+# Serviço systemctl
+ln -sf "$HTCOMET"/comet_zanthus.service /lib/systemd/system
+systemctl daemon-reload
 systemctl enable comet_zanthus
-service comet_zanthus start
+systemctl start comet_zanthus
+systemctl status comet_zanthus
 
-# Exibir apenas o PID do processo:
+##	arquivo comet_zanthus.service:
+
+#[Unit]
+#Description=Comet Zanthus
+#After=network.target remote-fs.target nss-lookup.target
+
+#[Service]
+#Type=forking
+#PIDFile=/var/lock/subsys/comet_zanthus
+#ExecStart=/usr/local/apache22/htdocs/comet_zanthus/comet_zanthus start
+#User=root
+#WorkingDirectory=/usr/local/apache22/htdocs/comet_zanthus
+#Restart=always
+#RestartSec=1s
+#StandardOutput=syslog
+#StandardError=inherit
+
+#[Install]
+#WantedBy=multi-user.target
 
 
-#/usr/bin/nohup /sbin/runuser -l root -c "/usr/local/bin/node /usr/local/apache22/htdocs/comet_zanthus/src/server-chat.js >> /usr/local/apache22/htdocs/comet_zanthus/src/comet_zanthus.log &" >> /dev/null
-#kill -9 `/bin/ps -aefw | /bin/grep "/usr/local/bin/node /usr/local/apache22/htdocs/comet_zanthus/src/server-chat.js" | /bin/grep -v "grep" | /bin/awk '{print $2}'`
+## Exibir apenas o PID do processo:
 
 #pgrep -f nome_do_processo
 #pidof nome_do_processo
@@ -38,4 +67,6 @@ service comet_zanthus start
 #Fonte:
 #https://pt.stackoverflow.com/questions/184743/como-posso-extrair-apenas-o-pid-de-um-processo
 #https://pt.stackoverflow.com/questions/246869/linux-leitura-correta-do-pid-linux
+
+
 
